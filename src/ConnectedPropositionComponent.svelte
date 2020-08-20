fc928f218363305240a2daebcf1429b4c9cd2886
Arnaud Oisel <arnaud.oisel@pm.me>
2020-08-20 09:30:38 +0200

    Add Connector component

--- src/ConnectedPropositionComponent.svelte
+++ src/ConnectedPropositionComponent.svelte
@@ -2,6 +2,7 @@
   import type { ConnectedProposition } from "./model/proposition/ConnectedProposition";
   import type { PropositionUnion } from "./model/proposition/PropositionUnion";
   import PropositionComponent from "./PropositionComponent.svelte";
+  import Connector from "./Connector.svelte";
   
   export let proposition: ConnectedProposition;
   let propositionsList: PropositionUnion[];
@@ -9,19 +10,37 @@
 </script>
 
 <div>
-  {#each propositionsList as propositionItem (propositionItem.id)}
+  {#each propositionsList as propositionItem, i (propositionItem.id)}
+    {#if i !== 0}
+      <Connector type={proposition.connectionType}/>
+    {/if}
     <PropositionComponent proposition={propositionItem}/>
   {/each}
-  <p> CONNECTED TO </p>
 </div>
 
 <style lang="scss">
   div {
     border: 1px solid grey;
     border-radius: 15px;
-    width: 200px;
     padding: 15px;
-    // text-align: justify;
-    // text-justify: inter-word;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    
+    > :global(div:nth-child(even)) {
+      margin: 0 10px 0 10px;
+    }
+  }
+
+  @media only screen and (min-width: 600px) {
+    div {
+      flex-direction: column;
+
+      > :global(div:nth-child(even)) {
+        margin: 10px 0 10px 0;
+      }
+    }
   }
 </style>
--- src/model/proposition/ConnectedProposition.ts
+++ src/model/proposition/ConnectedProposition.ts
@@ -1,11 +1,8 @@
 import type { Proposition } from "./Proposition";
 import type { PropositionType } from "./PropositionType";
 import type { PropositionUnion } from "./PropositionUnion";
+import type { ConnectionType } from "./ConnectionType";
 
-enum ConnectionType {
-  CONJUNCTION = 'conjunction',
-  DISJUNCTION = 'disjunction'
-}
 export interface ConnectedProposition extends Proposition {
   type: PropositionType.CONNECTION;
   connectionType: ConnectionType;
